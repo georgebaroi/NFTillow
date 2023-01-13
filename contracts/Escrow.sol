a25b79b038a475ac462ef1b8a65c5ef978f1ab78
George Baroi <baroi.george@gmail.com>
2023-01-12 19:16:14 -0500

    listings complete

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,50 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public lender;
+    address public inspector;
+    address payable public seller; 
+    address public nftAddress; 
+
+    modifier onlySeller(){
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice; 
+    mapping(uint256 => uint256) public escrowAmount;  
+    mapping(uint256 => address) public buyer;  
+
+    constructor(
+        address _nftAddress, 
+        address payable _seller, 
+        address _inspector, 
+        address _lender)
+        {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+
+
+    function list(
+        uint256 _nftID, 
+        address _buyer, 
+        uint256 _purchasePrice, 
+        uint256 _escrowAmount 
+        ) public payable onlySeller {
+        //transfer nft from seller to this contract 
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID); 
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount; 
+        buyer[_nftID] = _buyer; 
+    }
+
 
 }
+
